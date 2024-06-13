bb950860bcf2fcbf4d1544a049fef0174b5b2957
Vladislav Chernyshev <tchernyshev.vladik@yandex.ru>
2024-06-13 21:19:37 +0300

    v.0.0.2.3

--- Dockerfile
+++ Dockerfile
@@ -5,8 +5,12 @@ WORKDIR /code
 
 COPY ./requirements.txt /code/requirements.txt
 
+RUN apt-get update \
+    && apt-get install gcc -y \
+    && apt-get clean
+
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 
 COPY . /code/
 
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
+CMD ["uvicorn", "main:app", "--reload", "--host", "0.0.0.0", "--port", "8000"]
--- src/service/utils/db.py
+++ src/service/utils/db.py
@@ -3,15 +3,15 @@ from datetime import datetime
 from sqlalchemy import create_engine, TIMESTAMP, func
 from sqlalchemy.orm import sessionmaker, DeclarativeBase, mapped_column, Mapped, declared_attr
 
-from src.config.project_config.config import settings
+from src.config.project_config.config import settings, database
 from src.service.utils import models
 
 
 if settings.DEBUG:
     SQLALCHEMY_DATABASE_URL = "sqlite:///./vChernTestDB.db"
 else:
-    SQLALCHEMY_DATABASE_URL = f"{settings.DB_ENGINE}://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}/" + \
-                              f"{settings.DB_NAME}"
+    SQLALCHEMY_DATABASE_URL = f"{settings.DB_ENGINE}://{database.DB_USER}:{database.DB_PASSWORD}@{settings.DB_HOST}/" + \
+                              f"{database.DB_NAME}"
 # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123@localhost:5432/postgres"
 # SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:1@vchern.me:5432/postgres"
 
