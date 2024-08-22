8f27ff97a2238f5d1b16abe3e1e74616412083b0
Vladislav Chernyshev <tchernyshev.vladik@yandex.ru>
2024-08-22 19:48:11 +0300

    v.0.0.4.7

--- src/service/route/Main/comments.py
+++ src/service/route/Main/comments.py
@@ -16,7 +16,7 @@ async def read_comments(project_id: int,
         db: Session = Depends(get_db)):
     project = await crud.get_project_by_id(db, project_id)
     if not project:
-        return HTTPException(status_code=404, detail="Проект не найден!")
+        raise HTTPException(status_code=404, detail="Проект не найден!")
     return await crud.get_comments(db, project.id)
 
 
@@ -26,7 +26,7 @@ async def create_comment(current_user: Annotated[schemas.User, Depends(get_curre
                          db: Session = Depends(get_db)):
     project = await crud.get_project_by_id(db, comment.project_id)
     if not project:
-        return HTTPException(status_code=404, detail="Проект не найден!")
+        raise HTTPException(status_code=404, detail="Проект не найден!")
     return await crud.create_comment(db, comment, current_user)
 
 
@@ -36,10 +36,10 @@ async def update_comment(current_user: Annotated[schemas.User, Depends(get_curre
                          db: Session = Depends(get_db)):
     comment = await crud.get_comment_by_id(db, new_comment.comment_id)
     if not comment:
-        return HTTPException(status_code=404, detail="Комментарий не найден!")
+        raise HTTPException(status_code=404, detail="Комментарий не найден!")
     if comment.user_uuid == current_user.uuid or current_user.is_admin:
         return await crud.update_comment(db, comment, new_comment)
-    return HTTPException(status_code=403, detail="Данный пользователь не обладает нужными правами доступа!")
+    raise HTTPException(status_code=403, detail="Данный пользователь не обладает нужными правами доступа!")
 
 
 @router.delete("/{comment_id}/")
@@ -48,7 +48,7 @@ async def delete_comment(current_user: Annotated[schemas.User, Depends(get_curre
                         db: Session = Depends(get_db)):
     comment = await crud.get_comment_by_id(db, comment_id)
     if not comment:
-        return HTTPException(status_code=404, detail="Комментарий не найден!")
+        raise HTTPException(status_code=404, detail="Комментарий не найден!")
     if comment.user_uuid == current_user.uuid or current_user.is_admin:
         return await crud.delete_comment_by_id(db, comment)
-    return HTTPException(status_code=403, detail="Данный пользователь не обладает нужными правами доступа!")
+    raise HTTPException(status_code=403, detail="Данный пользователь не обладает нужными правами доступа!")
