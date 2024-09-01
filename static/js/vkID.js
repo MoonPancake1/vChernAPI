0edf69e4d46572550af1f59a8fa13c894110cd2d
Vladislav Chernyshev <tchernyshev.vladik@yandex.ru>
2024-09-01 18:05:17 +0300

    v.0.1.0.2

---
+++ static/js/vkID.js
@@ -0,0 +1,23 @@
+const VKID = window.VKIDSDK;
+
+VKID.Config.init({
+  app: 52237939,
+  redirectUrl: 'https://id.vchern.me/id/oauth/vk/',
+  mode: VKID.ConfigAuthMode.Redirect,
+});
+
+// Создание экземпляра кнопки.
+const oneTap = new VKID.OneTap();
+
+// Получение контейнера из разметки.
+const container = document.getElementById('VkIdSdkOneTap');
+
+// Проверка наличия кнопки в разметке.
+if (container) {
+  console.log("Работает", container)
+  // Отрисовка кнопки в контейнере с именем приложения APP_NAME, светлой темой и на русском языке.
+  oneTap.render({ container: container, scheme: VKID.Scheme.LIGHT, lang: VKID.Languages.RUS })
+        .on(VKID.WidgetEvents.ERROR, handleError); // handleError — какой-либо обработчик ошибки.
+} else {
+  console.log("Что-то не то", container)
+}
