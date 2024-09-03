a67234e3b81cfb712457b4bb0b2bc014c0948471
Vladislav Chernyshev <tchernyshev.vladik@yandex.ru>
2024-09-03 15:05:54 +0300

    v.0.1.0.4

--- static/js/vkID.js
+++ static/js/vkID.js
@@ -1,4 +1,4 @@
-function get_code_verifier (length) {
+function get_code_verifier(length) {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let result = '';
     const charactersLength = characters.length;
@@ -9,6 +9,18 @@ function get_code_verifier (length) {
     }
 
     return result;
+}
+
+function get_user_data(userTokens) {
+    VKID.Auth.userInfo(userTokens.access_token).then(
+        userData => {
+            fetch(
+                `https://id.vchern.me/id/oauth/vk/` + new URLSearchParams(userData).toString()
+            ).then(r => {
+                console.log(r)
+            })
+        }
+    )
 }
 
 const VKID = window.VKIDSDK;
@@ -36,10 +48,7 @@ if (urlParams.has('code')) {
     entries = urlParams.entries();
     const code = urlParams.get('code')
     const device_id = urlParams.get('device_id')
-    const userTokens = VKID.Auth.exchangeCode(code, device_id)
-    const userData = VKID.Auth.userInfo(userTokens.access_token)
-    console.log(userData)
-    fetch(
-        `https://id.vchern.me/id/oauth/vk/` + new URLSearchParams(userData).toString()
+    VKID.Auth.exchangeCode(code, device_id).then(
+        userTokens => get_user_data(userTokens)
     )
 }
